088e8326deb17bc85bd4f5fa7c9a7077fa98cf06
Srijan Baniyal <john.rambo.9901@gmail.com>
2025-11-04 18:19:24 +0530

    Small updates

--- src/routes/waitlist.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/waitlist')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/waitlist"!</div>
-}
--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
-    viteReact(),
+    viteReact()
   ],
 })
 
