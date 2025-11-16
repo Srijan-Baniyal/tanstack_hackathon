1e458acc539c2e1581a3e9fc581edea863375cce
Srijan Baniyal <john.rambo.9901@gmail.com>
2025-11-16 11:14:48 +0530

    fix: update import paths in signinandsignup.tsx for consistency

--- src/routes/signinandsignup.tsx
+++ src/routes/signinandsignup.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { SignInAndSignUp } from "@/components/SignInAndUp";
-import Navbar from "@/components/Navbar";
+import { SignInAndSignUp } from "../components/SignInAndUp";
+import Navbar from "../components/Navbar";
 
 export const Route = createFileRoute("/signinandsignup")({
   component: RouteComponent,
