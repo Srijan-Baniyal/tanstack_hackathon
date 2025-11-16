235b14fcf6e03f069def5601ba9a7d6df1a2a87d
Srijan Baniyal <john.rambo.9901@gmail.com>
2025-11-16 10:57:09 +0530

    I hope this deploys it

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import netlify from "@netlify/vite-plugin-tanstack-start"; // ← add this
-import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
-    netlify(), // ← add this (anywhere in the array is fine)
-    viteReact(),
+    netlify(),
   ],
-});
+})
